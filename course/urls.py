70f503712e784de59f466d0fd8187e2cb74d472b
soumyadeepdutta <imsoumyadeepdutta@gmail.com>
2020-11-03 19:56:24 +0530

    Account app added
    User registration

--- course/urls.py
+++ course/urls.py
@@ -6,5 +6,5 @@ app_name = 'course'
 urlpatterns = [
     path('', views.HomeView.as_view(), name="home"),
     path('courses', views.CourseListView.as_view(), name="courseList"),
-    path('courses/<int:pk>', views.CourseDetailView.as_view(), name="courseDetail")
+    path('courses/<int:pk>', views.CourseDetailView.as_view(), name="courseDetail"),
 ]
--- educative/urls.py
+++ educative/urls.py
@@ -2,12 +2,15 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+# from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('course.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('account.urls')),
 ]
+
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

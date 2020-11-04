8bba87559ba95c4b5fe57c53689f17001b02cad8
soumyadeepdutta <imsoumyadeepdutta@gmail.com>
2020-11-04 09:51:19 +0530

    Views fixed for course app

--- course/views.py
+++ course/views.py
@@ -1,11 +1,18 @@
 # from django.shortcuts import render
 from django.views.generic import DetailView, ListView
+from django.views.generic.list import ListView as LV
 from .models import Course
+from .models import Category
 
 
-class HomeView(ListView):
+class HomeView(LV):
     model = Course
     template_name = 'base/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 
 class CourseListView(ListView):

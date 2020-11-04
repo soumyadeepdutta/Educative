38551cb851d72632bfcdc65513e9fb57cf1d40c0
soumyadeepdutta <imsoumyadeepdutta@gmail.com>
2020-11-04 09:07:13 +0530

    Removed authentication from urls

--- account/views.py
+++ account/views.py
@@ -1,41 +1,20 @@
-# from django.contrib.auth.forms import UserCreationForm
-# from django.http import HttpResponse
-# from django.views.generic import CreateView
-# from django.urls import reverse_lazy
-from django.contrib.auth import authenticate, login
-from django.shortcuts import redirect, render
-
-from .forms import RegisterForm, StudentRegisterForm
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.views import View
+from django.contrib.auth.views import AuthenticationForm
 
 
-# need to be fixed
-# class StudentSignupView(CreateView):
-#     form_classes = {
-#         'register': RegisterForm,
-#         'student': StudentRegisterForm
-#     }
-#     template_name = 'register.html'
-#     success_url = reverse_lazy('course:home')
+class RegistrationView(CreateView):
+    form_class = UserCreationForm
+    # model = User
+    template_name = 'register.html'
+    print('registered')
+    success_url = reverse_lazy('course:courseList')
+    # fields = ('username', 'password1')
 
-def register(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        student = StudentRegisterForm(request.POST)
 
-        if form.is_valid() and student.is_valid():
-            user = form.save()
-            student_user = student.save(commit=False)  # don't save in db
-            student_user.user = user  # for the one to one key
-            student_user.save()
-
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-
-            redirect('course:home')
-    else:
-        form = RegisterForm()
-        student_form = StudentRegisterForm()
-        context = {'form': form, 'student_form': student_form}
-        return render(request, 'register.html', context)
+class LoginView(View):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+    success_url = reverse_lazy('course:courseList')

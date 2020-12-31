b7c14ea65382166de396ee953863d8e7c389918a
soumyadeepdutta <imsoumyadeepdutta@gmail.com>
2020-12-31 19:31:17 +0530

    Authentication view fixed

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
 from django import forms
 
+
 class SignupForm(UserCreationForm):
     first_name = forms.CharField(max_length=128)
     last_name = forms.CharField(max_length=128)
@@ -12,12 +13,20 @@ class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
-    
+
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['class'] = 'form-control'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
     # override
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password1"])
-        
+
         if commit:
             user.save()
 
@@ -27,5 +36,5 @@ class SignupForm(UserCreationForm):
         else:
             group = Group.objects.get(name='Student')
             user.groups.add(group)
-        
+
         return user
--- accounts/views.py
+++ accounts/views.py
@@ -10,15 +10,10 @@ from .forms import SignupForm
 # Create your views here.
 class SignupView(CreateView):
     form_class = SignupForm
-    template_name = 'login.html'
+    template_name = 'register.html'
     success_url = reverse_lazy('accounts:profile')
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["auth"] = 'Signup'
-        return context
-    
+
 
 class ProfileView(TemplateView):
     model = get_user_model()
     template_name = 'profile.html'
-
